fab08ae0b156b340a7423116a37991797cad59af
anatolii <batkovich327@gmail.com>
2025-08-04 15:14:16 +0200

    connect updated

--- bot.py
+++ bot.py
@@ -1,20 +1,32 @@
 import asyncio
 import logging
-import os
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.enums import ParseMode
+from handlers import main_menu, todo, weather, gpt_assistant
 from dotenv import load_dotenv
-from handlers import main_menu
+import os
 
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher()
-dp.include_router(main_menu.router)
+bot = Bot(
+    token=TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
+dp = Dispatcher(storage=MemoryStorage())
+
+dp.include_routers(
+    main_menu.router,
+    todo.router,
+    weather.router,
+    gpt_assistant.router,
+)
 
 async def main():
     logging.basicConfig(level=logging.INFO)
-    print("Starting bot...")
+    print("Бот запущено.")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
